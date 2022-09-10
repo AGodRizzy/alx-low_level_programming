424aa558814cce96381c20047265303a9fd25bc2
AGodRizzy <arinzeifedobi@gmail.com>
2022-09-09 19:40:59 -0700

    C 0x01 tasks

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+char base_16;
+
+for (base _16 = 40; base_16 <= 57; base_16++)
+{
+putchar(base_16);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
