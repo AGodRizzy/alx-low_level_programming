046e3f0de33b83ab36058ecbd88423eb5fb90b74
AGodRizzy <arinzeifedobi@gmail.com>
2022-09-15 17:18:25 -0700

    first commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,14 +5,12 @@
  *
  * Return: Always 0.
  */
-
 void more_numbers(void)
 {
 int i;
 int first_number;
 int second_number;
 int result;
-
 i = 0;
 result = 0;
 while (i < 10)
@@ -23,22 +21,17 @@ if (result < 10)
 {
 second_number = result;
 }
-
 else
 {
 first_number = result / 10;
 second_number = result % 10;
 _putchar (first_number + '0');
 }
-
 _putchar (second_number + '0');
-
 result++;
 }
 i++;
 result = 0;
 _putchar ('\n');
 }
-
-return (0);
 }
