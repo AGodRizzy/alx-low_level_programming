49bc828a1805aef62de6d5ed9a5e97679f79e07c
AGodRizzy <arinzeifedobi@gmail.com>
2022-09-30 21:42:01 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,145 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <ctype.h>
+
 #include <string.h>
 
+
+
 /**
- * main - program that adds positive numbers
- * @argc: This is the arguement count
- * @argv: This is the argument vector
+
+ * check_num - check - string there are digit
+
+ * @str: array str
+
  *
- * Return: 0;
- * */
-int main(int argc, char *argv[])
+
+ * Return; Always 0 (Success)
+
+ */
+
+
+
+int check_num(char *str)
+
+
+
 {
-	int sum = 0, i;
 
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			int b;
-			char *str;
+/*Declaring variables*/
 
-			str = argv[i];
-			for (b = 0; str[b] != '\0'; b++)
-			{
-				if (str[b] < 48 \\ str[b] > 57)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-		}
-	}
-	for (i = 1; i < argc; i++)
-	{
-		sum += atoi(argv[i]);
-	}
-	printf("%d\n", sum);
-	return (0);
+unsigned int count;
+
+
+
+count = 0;
+
+while (count < strlen(str)) /*count string*/
+
+
+
+{
+
+if (!isdigit(str[count])) /*check if str there are digit*/
+
+{
+
+return (0);
+
+}
+
+
+
+count++;
+
+}
+
+return (1);
+
+}
+
+
+
+/**
+
+ * main - Print the name of the program
+
+ * @argc: Count arguments
+
+ * @argv: Arguments
+
+ *
+
+ * return: Always 0 (Success)
+
+ */
+
+
+
+int main(int argc, char *argv[])
+
+
+
+{
+
+
+
+/*Declaring variables*/
+
+int count;
+
+int str_to_int;
+
+int sum = 0;
+
+
+
+count = 1;
+
+while (count < argc) /*Goes through the whole array*/
+
+{
+
+if(check_num(argv[count]))
+
+
+
+{
+
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+
+sum += str_to_int;
+
+}
+
+
+
+/*Condition if one of the number contains symbols that are not digits*/
+
+else
+
+{
+
+printf("Error\n");
+
+return (1);
+
+}
+
+
+
+count++;
+
+}
+
+
+
+printf("%d\n", sum); /*print sum*/
+
+
+
+return (0);
+
 }
